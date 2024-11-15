4a7e925c271376c1043abbb263091781e41ce029
Vitor Sales <contato.vst@gmail.com>
2024-11-15 00:28:51 -0300

    proposta inicial macro Kaizen lvl 15

--- tormenta20/macros/dano.lua
+++ tormenta20/macros/dano.lua
@@ -1,96 +1,103 @@
 local rolagem
-local final;
+local final
 local adicional, observacao
-local log;
-local tipoDano;
+local log
+local tipoDano
 
+-- Modificadores e bônus fixos
 local temporario = 0
-local duasMaos = 5
-local machado = 2
-local modificador = 3
-local poderoso = 5
-local especialBaseMetade = 6
-local especialBaseTotal = 12
-local meioBonus = 4
-local bonusTotal = 8
-local valor = "";
-local adicional = 0;
+local duasMaos = 5 -- Bônus de Estilo de Duas Mãos
+local machado = 2 -- Bônus de Foco em Arma para o machado
+local modificador = 4 -- Modificador de Força do personagem
+local poderoso = 5 -- Ataque Poderoso
+local especialBaseMetade = 8 -- Bônus metade especial
+local especialBaseTotal = 16 -- Bônus total especial
+local meioBonus = 4 -- Meio bônus adicional para golpes especiais
+local bonusTotal = 8 -- Bônus total adicional para golpes especiais
+local valor = ""
+local adicional = 0
 
-local indice, texto = choose("Escolha o tipo de dano",{
+-- Escolha do tipo de dano
+local indice, texto = choose("Escolha o tipo de dano", {
     "Dano",
     "Especial 50% s/bônus",
     "Especial 100% s/bônus",
-    "Golpe de raspão",
     "Especial 50% c/bônus",
     "Especial 100% c/bônus",
     "Crítico",
     "Crítico especial 50% s/bônus",
     "Crítico especial 100% s/bônus",
     "Crítico especial 50% c/bônus",
-    "Crítico especial 100% c/bônus"}, 1);
+    "Crítico especial 100% c/bônus"
+}, 1)
 
 chat:escrever("Opção escolhida: " .. texto, true, true)
+tipoDano = indice
 
-tipoDano = indice;
+-- Entrada para bônus adicional
+extra = tonumber(inputQuery("Digite o valor extra", "0"))
+extra = tonumber(extra) or 0
 
-  extra =  tonumber(inputQuery("Digite o valor extra","0"))
-  extra = tonumber(extra) or 0
-
-
+-- Lógica para definir o dano com base no tipo de ataque selecionado
 if tipoDano == 1 then
   adicional = machado + modificador + duasMaos + poderoso + temporario + extra -- Adicional para o dano padrão
   log = "duasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra
   observacao = "DANO\n" .. log
-  valor = "1d12+";
+  valor = "1d12+"
+
 elseif tipoDano == 2 then
-  adicional = especialBaseMetade + machado + modificador + duasMaos  + poderoso + temporario + extra-- Adicional para o dano metade especial sem bônus
+  adicional = especialBaseMetade + machado + modificador + duasMaos + poderoso + temporario + extra -- Adicional para o dano metade especial sem bônus
   log = "especial: " .. especialBaseMetade .. "\nduasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra
-  observacao = "DANO ESPECIAL\n" .. log
-  valor = "1d12+";
+  observacao = "DANO ESPECIAL 50% S/BÔNUS\n" .. log
+  valor = "1d12+"
+
 elseif tipoDano == 3 then
-  adicional = especialBaseTotal + machado + modificador + duasMaos  + poderoso + temporario + extra-- Adicional para o dano especial sem bônus
+  adicional = especialBaseTotal + machado + modificador + duasMaos + poderoso + temporario + extra -- Adicional para o dano especial sem bônus
   log = "especial: " .. especialBaseTotal .. "\nduasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra
-  observacao = "DANO ESPECIAL\n" .. log
-  valor = "1d12+";
+  observacao = "DANO ESPECIAL 100% S/BÔNUS\n" .. log
+  valor = "1d12+"
+
 elseif tipoDano == 4 then
-  log = "1d8";
-  observacao = "RASPÃO";
- valor = "1d8+";
+  adicional = machado + modificador + duasMaos + poderoso + especialBaseMetade + meioBonus + temporario + extra -- Adicional para o dano especial com meio bônus
+  log = "especial: " .. especialBaseMetade .. "\nespecialBonus: " .. meioBonus .. "\nduasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra
+  observacao = "DANO ESPECIAL 50% C/BÔNUS\n" .. log
+  valor = "1d12+"
+
 elseif tipoDano == 5 then
-  adicional = machado + modificador + duasMaos  + poderoso  + especialBaseMetade + meioBonus + temporario + extra-- Adicional para o dano especial com meio bônus
-  log = "especial: " .. especialBaseMetade .. "\nespecialBonus: " .. meioBonus .. "\nduasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra 
-  observacao = "DANO ESPECIAL\n"  .. log
-  valor = "1d12+";
-elseif tipoDano == 6 then
-  adicional = machado + modificador + duasMaos  + poderoso + especialBaseTotal + bonusTotal + temporario + extra-- Adicional para o dano especial com bônus total 
-  log = "especial: " .. especialBaseTotal .. "\nespecialBonus: " .. bonusTotal .. "\nduasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra 
-  observacao = "DANO ESPECIAL\n"  .. log 
-  valor = "1d12+";
-elseif tipoDano == 7 then
-  adicional = machado + modificador + duasMaos + poderoso + temporario + extra-- Adicional para o dano crítico padrão
-  log = "duasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra
-  observacao = "DANO CRÍTICO\n"  .. log
-  valor = "3d12+";
-elseif tipoDano == 8 then
-  adicional = especialBaseMetade + machado + modificador + duasMaos  + poderoso + temporario + extra-- Adicional para o dano crítico sem bônus
-  log = "especial: " .. especialBaseMetade .. "\nduasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra
-  observacao = "DANO CRÍTICO\n"  .. log
-  valor = "3d12+";
-elseif tipoDano == 9 then
-  adicional = especialBaseTotal + machado + modificador + duasMaos  + poderoso + temporario + extra-- Adicional para o dano crítico sem bônus
-  log = "especial: " .. especialBaseTotal .. "\nduasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra
-  observacao = "DANO CRÍTICO\n"  .. log
-  valor = "3d12+";
-elseif tipoDano == 10 then
-  adicional = especialBaseMetade + machado + modificador + duasMaos  + poderoso + meioBonus + temporario + extra-- Adicional para o dano crítico crítico com meio bônus
-  log = "especial: " .. especialBaseMetade .. " \nespecialBonus: " .. meioBonus .. "\nduasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra
-  observacao = "DANO CRÍTICO\n"  .. log
-  valor = "3d12+";
-elseif tipoDano == 11 then
-  adicional = especialBaseTotal + machado +  modificador + duasMaos  + poderoso + bonusTotal + temporario + extra -- Adicional para o dano crítico crítico com bônus total
+  adicional = machado + modificador + duasMaos + poderoso + especialBaseTotal + bonusTotal + temporario + extra -- Adicional para o dano especial com bônus total
   log = "especial: " .. especialBaseTotal .. "\nespecialBonus: " .. bonusTotal .. "\nduasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra
-  observacao = "DANO CRÍTICO\n"  .. log
-  valor = "3d12+";
+  observacao = "DANO ESPECIAL 100% C/BÔNUS\n" .. log
+  valor = "1d12+"
+
+elseif tipoDano == 6 then
+  adicional = machado + modificador + duasMaos + poderoso + temporario + extra -- Adicional para o dano crítico padrão
+  log = "duasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra
+  observacao = "DANO CRÍTICO\n" .. log
+  valor = "3d12+"
+
+elseif tipoDano == 7 then
+  adicional = especialBaseMetade + machado + modificador + duasMaos + poderoso + temporario + extra -- Adicional para o dano crítico sem bônus
+  log = "especial: " .. especialBaseMetade .. "\nduasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra
+  observacao = "DANO CRÍTICO ESPECIAL 50% S/BÔNUS\n" .. log
+  valor = "3d12+"
+
+elseif tipoDano == 8 then
+  adicional = especialBaseTotal + machado + modificador + duasMaos + poderoso + temporario + extra -- Adicional para o dano crítico sem bônus
+  log = "especial: " .. especialBaseTotal .. "\nduasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra
+  observacao = "DANO CRÍTICO ESPECIAL 100% S/BÔNUS\n" .. log
+  valor = "3d12+"
+
+elseif tipoDano == 9 then
+  adicional = especialBaseMetade + machado + modificador + duasMaos + poderoso + meioBonus + temporario + extra -- Adicional para o dano crítico com meio bônus
+  log = "especial: " .. especialBaseMetade .. " \nespecialBonus: " .. meioBonus .. "\nduasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra
+  observacao = "DANO CRÍTICO ESPECIAL 50% C/BÔNUS\n" .. log
+  valor = "3d12+"
+
+elseif tipoDano == 10 then
+  adicional = especialBaseTotal + machado + modificador + duasMaos + poderoso + bonusTotal + temporario + extra -- Adicional para o dano crítico com bônus total
+  log = "especial: " .. especialBaseTotal .. "\nespecialBonus: " .. bonusTotal .. "\nduasMaos: " .. duasMaos .. "\nmodificador: " .. modificador .. "\nmachado: " .. machado .. "\npoderoso: " .. poderoso .. "\ntemporario: " .. temporario .. "\nextra: " .. extra
+  observacao = "DANO CRÍTICO ESPECIAL 100% C/BÔNUS\n" .. log
+  valor = "3d12+"
 end
 
 local resultado = rolar(valor .. adicional, observacao)
--- tormenta20/macros/teste-de-ataque.lua
+++ tormenta20/macros/teste-de-ataque.lua
@@ -1,23 +1,23 @@
-local resultado;
-local adicional;
-local observacao;
-local bonus;
-local tipoAtaque;
+local resultado
+local adicional
+local observacao
+local bonus
+local tipoAtaque
+local pmRestantes = 62 -- PM total do personagem
 
+-- Modificadores e bônus fixos
 local Heredrimm = 2
 local luta = 12
 local poderoso = -2
 local investida = 2
 local flanquado = 2
-local especialBaseMetade = 6
-local especialBaseTotal = 12
-local especialParcial = 4
-local especialTotal = 8 
+local especialBaseMetade = 8 -- Metade do bônus progressivo para Ataque Especial
+local especialBaseTotal = 16 -- Bônus total para Ataque Especial
+local foco = 4 -- Ajustado com Foco em Arma (+2)
 local temporario = 0
-local foco = 2
 
-alert('!!!!!!        COMUNIQUE ATAQUE PODEROSO       !!!!!!')
-local indice, texto = choose("!!!!!!        COMUNIQUE ATAQUE PODEROSO       !!!!!!", {
+-- Escolha do tipo de ataque
+local indice, texto = choose("Escolha o tipo de ataque:", {
   "Ataque: 0 PM",
   "Investida: 0 PM",
   "Flanqueado: 0 PM",
@@ -25,50 +25,60 @@ local indice, texto = choose("!!!!!!        COMUNIQUE ATAQUE PODEROSO       !!!!
   "Especial: 100% s/bônus -1 PM",
   "Especial 50% c/bônus: -3 PM",
   "Especial 100% c/bônus: -3 PM"
-}, 1);
+}, 1)
 
 chat:escrever("Opção escolhida: " .. texto, true, true)
+tipoAtaque = indice
 
-tipoAtaque = indice;
-
-bonus = tonumber(inputQuery("Digite o valor bonus","0"))
+-- Entrada para bônus adicional
+bonus = tonumber(inputQuery("Digite o valor bônus", "0"))
 bonus = tonumber(bonus) or 0
 
+-- Ataque Especial com progressão de PM gasto
+local pmEspecial = tonumber(inputQuery("PM a gastar no Ataque Especial (0 a 4)", "0"))
+pmEspecial = math.min(pmEspecial, 4)
+especialBaseTotal = 4 * pmEspecial
+especialBaseMetade = 2 * pmEspecial
+
+-- Condição de Valentão
+local valentao = tonumber(inputQuery("O alvo está caído, desprevenido ou flanqueado? (Sim = 2, Não = 0)", "0"))
+
+-- Lógica para definir bônus de ataque com base no tipo de ataque selecionado
 if tipoAtaque == 1 then
-  adicional = Heredrimm + luta + poderoso + bonus + temporario + foco
-  log = "\nHeredrimm: " .. Heredrimm .. "\nluta: " .. luta .. "\npoderoso: " .. poderoso .. "\nbonus: " .. bonus .. "\ntemporario: " .. temporario .. "\nfoco: " .. foco
-  observacao = " Ataque poderoso: 0 PM" .. log
+  adicional = Heredrimm + luta + poderoso + bonus + temporario + foco + valentao
+  observacao = "Ataque poderoso: 0 PM"
+
 elseif tipoAtaque == 2 then
-  adicional = Heredrimm + luta + investida + poderoso + bonus + temporario + foco
-  observacao = " - Investida poderosa: 0 PM"
-  log = " Heredrimm: " .. Heredrimm .. "\nluta: " .. luta .. "\npoderoso: " .. poderoso .. "\nbonus: " .. bonus .. "\ntemporario: " .. temporario .. "\ninvestida: " .. investida .. "\nfoco: " .. foco
+  adicional = Heredrimm + luta + investida + poderoso + bonus + temporario + foco + valentao
+  observacao = "Investida poderosa: 0 PM"
+
 elseif tipoAtaque == 3 then
-  adicional = Heredrimm + luta + flanquado + poderoso + bonus + temporario + foco
-  observacao = " - Flanqueamento poderoso: 0 PM"
-  log = " Heredrimm: " .. Heredrimm .. "\nluta: " .. luta .. "\npoderoso: " .. poderoso .. "\nbonus: " .. bonus .. "\ntemporario: " .. temporario .. "\nflanquado: " .. flanquado .. "\nfoco: " .. foco
+  adicional = Heredrimm + luta + flanquado + poderoso + bonus + temporario + foco + valentao
+  observacao = "Flanqueamento poderoso: 0 PM"
+
 elseif tipoAtaque == 4 then
-  adicional = Heredrimm + luta + poderoso + bonus + temporario + especialBaseMetade + foco
-  observacao = " - Especial poderoso 50% s/bônus: -1PM"
-  log = " Heredrimm: " .. Heredrimm .. "\nluta: " .. luta .. "\npoderoso: " .. poderoso .. "\nbonus: " .. bonus .. "\ntemporario: " .. temporario .. "\nespecial: " .. especialBaseMetade .. "\nfoco: " .. foco
+  adicional = Heredrimm + luta + poderoso + bonus + temporario + especialBaseMetade + foco + valentao
+  observacao = "Especial poderoso 50% s/bônus: -1 PM"
+
 elseif tipoAtaque == 5 then
-  adicional = Heredrimm + luta + poderoso + bonus + temporario + especialBaseTotal + foco
-  observacao = " - Especial poderoso 100% s/bônus: -1PM"
-  log = " Heredrimm: " .. Heredrimm .. "\nluta: " .. luta .. "\npoderoso: " .. poderoso .. "\nbonus: " .. bonus .. "\ntemporario: " .. temporario .. "\nespecial: " .. especialBaseTotal .. "\nfoco: " .. foco
+  adicional = Heredrimm + luta + poderoso + bonus + temporario + especialBaseTotal + foco + valentao
+  observacao = "Especial poderoso 100% s/bônus: -1 PM"
+
 elseif tipoAtaque == 6 then
-  adicional = Heredrimm + luta + especialBaseMetade + especialParcial + poderoso + bonus + temporario + foco
-  observacao = " - Especial poderoso + 50% bônus: -3PM"
-  log = " Heredrimm: " .. Heredrimm .. "\nluta: " .. luta .. "\npoderoso: " .. poderoso .. "\nbonus: " .. bonus .. "\ntemporario: " .. temporario .. "\nespecial: " .. especialBaseMetade .. "\nbonusEspecial: " .. especialParcial .. "\nfoco: " .. foco
+  adicional = Heredrimm + luta + especialBaseMetade + especialParcial + poderoso + bonus + temporario + foco + valentao
+  observacao = "Especial poderoso 50% c/bônus: -3 PM"
+
 elseif tipoAtaque == 7 then
-  adicional = Heredrimm + luta + especialBaseTotal + especialTotal + poderoso + bonus + temporario + foco
-  observacao = " Especial poderoso + 100% bônus: -3PM"
-  log = " Heredrimm: " .. Heredrimm .. "\nluta: " .. luta .. "\npoderoso: " .. poderoso .. "\nbonus: " .. bonus .. "\ntemporario: " .. temporario .. "\nespecial: " .. especialBaseTotal .. "\nespecialBonus: " .. especialTotal .. "\nfoco: " .. foco
+  adicional = Heredrimm + luta + especialBaseTotal + especialTotal + poderoso + bonus + temporario + foco + valentao
+  observacao = "Especial poderoso 100% c/bônus: -3 PM"
 end
 
+-- Cálculo do ataque e rolagem
 local resultado = rolar("1d20+" .. adicional, observacao)
-
 local valorSemModificador = tonumber(resultado) - tonumber(adicional)
 
-if valorSemModificador == 19 then
+-- Check de crítico
+if valorSemModificador >= 19 then
   enviar("💥 [§K7] [KAIZEN] [§K4] ACERTOU UM CRÍTICO!")
 elseif valorSemModificador > 1 then
   enviar("🪓 [§K7] [KAIZEN] [§K1]" .. resultado .. "[§K1] te acerta? ")
@@ -76,3 +86,13 @@ else
   enviar("❌ [§K1] FALHA =/ [§K7] [KAIZEN]  [§K1] ERROU ")
 end
 
+-- Opção para usar Mestre em Arma para rerolar
+local reroll = choose("Usar Mestre em Arma para rerolar o ataque? (-2 PM)", {"Sim", "Não"}, 2)
+if reroll == 1 then
+  if pmRestantes >= 2 then
+    pmRestantes = pmRestantes - 2
+    resultado = rolar("1d20+" .. adicional, " - Rerol após Mestre em Arma (-2 PM)")
+  else
+    enviar("⚠️ [§K4] PM insuficientes para usar Mestre em Arma! (Necessário 2 PM)")
+  end
+end
